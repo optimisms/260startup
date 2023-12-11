7171b0537bb291a5b1488cc483af8639e3e19e4f
optimisms <optimisms@users.noreply.github.com>
2023-12-11 13:35:15 -0700

    Potentially reset to working state after breaking newform

---
+++ index.js
@@ -0,0 +1,45 @@
+let history = [];
+
+const express = require('express');
+const app = express();
+
+// Set up listening port
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
+
+// Serve index.html as default page
+// app.use((_req, res) => {
+//     res.sendFile('html/index.html', { root: 'public' });
+// });
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Router for service endpoints
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// GetHistory
+apiRouter.get('/history', (req, res) => {
+    res.send(history);
+});
+
+// SubmitNewForm
+apiRouter.post('/form', (req, res) => {
+    console.log('POST request received at /api/form');
+    history = updateHistory(req.body, history);
+    //TODO: Push to DB
+    res.send(history);
+});
+
+// updateHistory adds a new form to the history and returns the updated history.
+function updateHistory(newForm, history) {
+    history.push(newForm);
+
+    return history;
+}
