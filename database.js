10edac9644082906ade4916731d2bc8421bfad3f
optimisms <optimisms@users.noreply.github.com>
2023-12-12 13:04:36 -0700

    Moved to new permanent database

--- database.js
+++ database.js
@@ -3,17 +3,13 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('testDB');
-const collection = db.collection('testCollection');
-
-const newDB = client.db('startup');
-const newCollection = newDB.collection('formHistory');
+const db = client.db('startup');
+const collection = db.collection('formHistory');
 
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
-  await newDB.command({ ping: 1 });
-  console.log("Connected successfully to both DB servers");
+  console.log("Connected successfully to DB server");
 })().catch((ex) => {
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
@@ -26,19 +22,16 @@ async function submitNewForm(form) {
     return result;
 }
   
-async function getHistory() {
+function getHistory() {
     console.log("Entered getHistory()");
 
     const query = {};
     const options = {
         sort: { date: -1 },
     };
-    const cursor = await collection.find(query, options).toArray();
+    const cursor = collection.find(query, options);
 
-    console.log(cursor);
-    await newCollection.insertMany(cursor);
-
-    return cursor;
+    return cursor.toArray();
 }
   
   module.exports = { submitNewForm, getHistory };
