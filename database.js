e15f20ec292a83f8ccce3e3008aa99760cc83978
optimisms <optimisms@users.noreply.github.com>
2023-12-12 12:17:36 -0700

    Copied data from testDB to real DB

--- database.js
+++ database.js
@@ -6,10 +6,14 @@ const client = new MongoClient(url);
 const db = client.db('testDB');
 const collection = db.collection('testCollection');
 
+const newDB = client.db('startup');
+const newCollection = newDB.collection('formHistory');
+
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
-  console.log("Connected successfully to DB server");
+  await newDB.command({ ping: 1 });
+  console.log("Connected successfully to both DB servers");
 })().catch((ex) => {
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
@@ -22,15 +26,19 @@ async function submitNewForm(form) {
     return result;
 }
   
-function getHistory() {
+async function getHistory() {
     console.log("Entered getHistory()");
 
     const query = {};
     const options = {
         sort: { date: -1 },
     };
-    const cursor = collection.find(query, options);
-    return cursor.toArray();
+    const cursor = await collection.find(query, options).toArray();
+
+    console.log(cursor);
+    await newCollection.insertMany(cursor);
+
+    return cursor;
 }
   
   module.exports = { submitNewForm, getHistory };
