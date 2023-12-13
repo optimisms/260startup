826b52eb84f5745c88df5726a00ab4fa913ae6ea
optimisms <optimisms@users.noreply.github.com>
2023-12-12 23:54:19 -0700

    Minor edits to debug

--- public/js/login.js
+++ public/js/login.js
@@ -10,48 +10,48 @@
     // }
   })();
 
-function login() {
-    const nameEl = document.querySelector("#userName");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "history.html";
-}
+// function login() {
+//     const nameEl = document.querySelector("#userName");
+//     localStorage.setItem("userName", nameEl.value);
+//     window.location.href = "history.html";
+// }
 
-function register() {
-    const nameEl = document.querySelector("#userName");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "history.html";
-}
+// function register() {
+//     const nameEl = document.querySelector("#userName");
+//     localStorage.setItem("userName", nameEl.value);
+//     window.location.href = "history.html";
+// }
 
-async function loginUser() {
-    loginOrCreate(`/api/auth/login`);
-  }
+async function login() {
+    loginOrCreate('/api/auth/login');
+}
   
-  async function createUser() {
-    loginOrCreate(`/api/auth/create`);
-  }
+async function register() {
+    loginOrCreate('/api/auth/create');
+}
   
-  async function loginOrCreate(endpoint) {
+async function loginOrCreate(endpoint) {
     const username = document.querySelector('#userName')?.value;
     const password = document.querySelector('#userPassword')?.value;
     const response = await fetch(endpoint, {
-      method: 'post',
-      body: JSON.stringify({ username: username, password: password }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
     });
   
     if (response.ok) {
-      localStorage.setItem('userName', username);
-      window.location.href = 'history.html';
+        localStorage.setItem('userName', username);
+        window.location.href = 'history.html';
     } else {
-      const body = await response.json();
-      const modalEl = document.querySelector('#msgModal');
-      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
-      const msgModal = new bootstrap.Modal(modalEl, {});
-      msgModal.show();
+        const body = await response.json();
+        const modalEl = document.querySelector('#msgModal');
+        modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+        const msgModal = new bootstrap.Modal(modalEl, {});
+        msgModal.show();
     }
-  }
+}
 
 function seeRecords() {
     window.location.href = "history.html";
