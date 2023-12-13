a4020bcb37d607850e79635ea79ad195a8b42923
optimisms <optimisms@users.noreply.github.com>
2023-12-12 23:14:56 -0700

    Replaced old login code with simon code bc I couldn't figure out how to fix broken form in old code

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,22 @@
+// (async () => {
+//     const userName = localStorage.getItem('userName');
+//     if (userName) {
+//       setDisplay('loginControls', 'none');
+//       setDisplay('playControls', 'block');
+//     } else {
+//       setDisplay('loginControls', 'block');
+//       setDisplay('playControls', 'none');
+//     }
+//   })();
+
 function login() {
-    const nameEl = document.querySelector("#name");
+    const nameEl = document.querySelector("#userName");
+    localStorage.setItem("userName", nameEl.value);
+    window.location.href = "history.html";
+}
+
+function register() {
+    const nameEl = document.querySelector("#userName");
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "history.html";
 }
