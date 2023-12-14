17c39738dd02a7b4185a9a86e545558768bd08fe
optimisms <optimisms@users.noreply.github.com>
2023-12-13 22:14:49 -0700

    Fixed var name

--- peerProxy.js
+++ peerProxy.js
@@ -6,7 +6,7 @@ function peerProxy(httpServer) {
     const wss = new WebSocketServer({ noServer: true });
 
 	// Handle the protocol upgrade from HTTP to WebSocket
-	server.on('upgrade', (request, socket, head) => {
+	httpServer.on('upgrade', (request, socket, head) => {
 		wss.handleUpgrade(request, socket, head, function done(ws) {
 			wss.emit('connection', ws, request);
 		});
