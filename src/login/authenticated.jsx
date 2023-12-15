f01b7e057868750af78b76bf3306f6dde6b53ba6
optimisms <optimisms@users.noreply.github.com>
2023-12-14 21:31:44 -0700

    Added (broken) buttons

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -4,6 +4,11 @@ export default function Authenticated() {
     return (
         <div>
             <h1>Authenticated goes here.</h1>
+            <div id="playControls" style="display: block">
+                <div id="userNameDisplay"></div>
+                <button type="button" className="btn btn-primary" onClick="seeRecords()">See Records</button>
+                <button type="button" className="btn btn-secondary" onClick="logout()">Logout</button>
+            </div>
         </div>
     )
 };
