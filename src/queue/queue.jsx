a99d9edfca97f6108c2f75ff332c710ee64718f0
optimisms <optimisms@users.noreply.github.com>
2023-12-14 22:45:21 -0700

    Added Queue html

--- src/queue/queue.jsx
+++ src/queue/queue.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './queue.css';
 
 export function Queue() {
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>queue displayed here</div>
+      <div id="alert_container"></div>
+      <div id="queue">
+        <div id="queue_info">
+          You are 
+          <h2>#<span id="curr_pos_info">10</span></h2>
+          out of <span id="queue_len">35</span>
+        </div>
+        <br />
+        <div id="time">
+          Est. time remaining: <span id="time_remaining">20 min</span>
+        </div>
+      </div>
     </main>
   );
 }
