8122a857e072541a23c8a1314541d3121966c17b
optimisms <optimisms@users.noreply.github.com>
2023-12-14 22:37:52 -0700

    Added props to Unauthenticated (?)

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 
 import Unauthenticated from './unauthenticated';
 import Authenticated from './authenticated';
+import { AuthState } from './authState';
+
 import "./login.css";
-// import { AuthState } from './authState';
 
 export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className='container-fluid bg-secondary text-center'>
       <h1>Welcome to the PGSPCA</h1>
       <div id="picture" className="picture-box"><img width="200px" src="../../public/dog.jpeg" alt="random" /></div>
-      <Authenticated />
-      <Unauthenticated />
+      <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+      <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+      />
     </main>
   );
 }
