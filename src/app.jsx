dedf4525bffed830500edde71c6c36ef92d1f987
optimisms <optimisms@users.noreply.github.com>
2023-12-14 17:52:33 -0700

    Added header/footer to App component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+    return (
+      <div className='body bg-dark text-light'>
+        <header className="container-fluid">
+            <nav className="navbar fixed-top navbar-dark">
+                <a className="navbar-brand" href="#">PGSPCA</a>
+                <menu className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="history.html">History</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="newform.html">New Form</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="queue.html">Queue</a>
+                    </li>
+                </menu>
+            </nav>
+        </header>
+  
+        <main>App components go here</main>
+  
+        <footer class="bg-dark text-white-50">
+            <div class="container-fluid">
+                <span class="text-reset">Haile Terry</span>
+                <a class="text-reset" href="https://github.com/optimisms/startup" target="_blank">GitHub</a>
+            </div>
+        </footer>
+      </div>
+    );
+  }
