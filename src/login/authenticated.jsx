8d4868d179e4dd9e441d9c05864365ddc0281640
optimisms <optimisms@users.noreply.github.com>
2023-12-14 21:47:33 -0700

    Fixed some errors preventing render

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
-import "authenticated.css";
+import "./authenticated.css";
 
-export default function Authenticated() {
+export default function Authenticated(props) {
     const navigate = useNavigate();
 
     function logout() {
@@ -22,12 +22,9 @@ export default function Authenticated() {
 
     return (
         <div>
-            <h1>Authenticated goes here.</h1>
-            <div id="playControls" style="display: block">
-                <div id="userNameDisplay"></div>
-                <Button variant='primary' onClick={() => navigate('/history')}>See Records</Button>
-                <Button variant='secondary' onClick={() => logout()}>Logout</Button>
-            </div>
+            <div id="userNameDisplay"></div>
+            <Button variant='primary' onClick={() => navigate('/history')}>See Records</Button>
+            <Button variant='secondary' onClick={() => logout()}>Logout</Button>
         </div>
     )
 };
