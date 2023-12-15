96e522accabec642e2fbd5e03b108b2320686f85
optimisms <optimisms@users.noreply.github.com>
2023-12-14 21:24:05 -0700

    Added dog pic to login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Unauthenticated from './unauthenticated';
 import Authenticated from './authenticated';
+import "./login.css";
 // import { AuthState } from './authState';
 
 export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className='container-fluid bg-secondary text-center'>
       <h1>Welcome to the PGSPCA</h1>
+      <div id="picture" className="picture-box"><img width="200px" src="../../public/dog.jpeg" alt="random" /></div>
       <Authenticated />
       <Unauthenticated />
     </main>
