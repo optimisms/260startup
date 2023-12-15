64dd582bbdacb0cf0014baf0dd4b04bbed42d09a
optimisms <optimisms@users.noreply.github.com>
2023-12-14 22:27:17 -0700

    Added html to Unauthenticated

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export default function Unauthenticated() {
+export default function Unauthenticated(props) {
+    const [userName, setUserName] = React.useState(props.userName);
+    const [password, setPassword] = React.useState('');
+
     return (
         <div>
-            <h1>Unauthenticated goes here.</h1>
+                <p>Login to view your records</p>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">@</span>
+                    <input className="form-control" type="text" id="userName" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="username" />
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">🔒</span>
+                    <input className="form-control" type="password" id="userPassword" onChange={(e) => setPassword(e.target.value)} placeholder="password" />
+                </div>
+                {/* <button type="button" className="btn btn-primary" onClick="login()">Login</button> */}
+                {/* <button type="button" className="btn btn-primary" onClick="register()">Create</button> */}
+            
         </div>
     )
 };
