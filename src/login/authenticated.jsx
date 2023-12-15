93f8134e6523e9f378fdb7dcb3ecbd2fb471af74
optimisms <optimisms@users.noreply.github.com>
2023-12-14 21:37:21 -0700

    Added navigate function

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 import "authenticated.css";
 
 export default function Authenticated() {
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1>Authenticated goes here.</h1>
             <div id="playControls" style="display: block">
                 <div id="userNameDisplay"></div>
-                <button type="button" className="btn btn-primary" onClick="seeRecords()">See Records</button>
-                <button type="button" className="btn btn-secondary" onClick="logout()">Logout</button>
-
-                <Button variant='primary' onClick={() => navigate('/play')}>See Records</Button>
+                <Button variant='primary' onClick={() => navigate('/history')}>See Records</Button>
                 <Button variant='secondary' onClick={() => logout()}>Logout</Button>
             </div>
         </div>
