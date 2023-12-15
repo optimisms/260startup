a5582c349c6dfa2b229f7968d70611d86d141aa3
optimisms <optimisms@users.noreply.github.com>
2023-12-14 18:13:57 -0700

    Added BrowserRouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { History } from './history/history';
+import { NewForm } from './newform/newform';
+import { Queue } from './queue/queue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
     return (
-      <div className='body bg-dark text-light'>
-        <header className="container-fluid">
-            <nav className="navbar fixed-top navbar-dark">
-                <a className="navbar-brand" href="#">PGSPCA</a>
-                <menu className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="index.html">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="history.html">History</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="newform.html">New Form</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="queue.html">Queue</a>
-                    </li>
-                </menu>
-            </nav>
-        </header>
-  
-        <main>App components go here</main>
-  
-        <footer class="bg-dark text-white-50">
-            <div class="container-fluid">
-                <span class="text-reset">Haile Terry</span>
-                <a class="text-reset" href="https://github.com/optimisms/startup" target="_blank">GitHub</a>
+        <BrowserRouter>
+            <div className='body bg-dark text-light'>
+                <header className="container-fluid">
+                    <nav className="navbar fixed-top navbar-dark">
+                        <a className="navbar-brand" href="#">PGSPCA</a>
+                        <menu className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="index.html">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="history.html">History</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="newform.html">New Form</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="queue.html">Queue</a>
+                            </li>
+                        </menu>
+                    </nav>
+                </header>
+        
+                <main>App components go here</main>
+        
+                <footer class="bg-dark text-white-50">
+                    <div class="container-fluid">
+                        <span class="text-reset">Haile Terry</span>
+                        <a class="text-reset" href="https://github.com/optimisms/startup" target="_blank">GitHub</a>
+                    </div>
+                </footer>
             </div>
-        </footer>
-      </div>
+        </BrowserRouter>
     );
   }
