f2d31946bbb31a96452c364f5c8c6a5dd039db04
optimisms <optimisms@users.noreply.github.com>
2023-12-14 22:33:39 -0700

    Added js functionality to Unauthenticated

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -3,8 +3,32 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 export default function Unauthenticated(props) {
-    const [userName, setUserName] = React.useState(props.userName);
+    const [userName, setUserName] = React.useState(props.userName || '');
     const [password, setPassword] = React.useState('');
+
+    async function loginUser() {
+        loginOrCreate(`/api/auth/login`);
+    }
+    
+    async function createUser() {
+        loginOrCreate(`/api/auth/register`);
+    }
+
+    async function loginOrCreate(endpoint) {
+        const response = await fetch(endpoint, {
+            method: 'post',
+            body: JSON.stringify({username: userName, password: password}),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        if (response?.status === 200) {
+            localStorage.setItem('userName', userName);
+            props.onLogin(userName);
+        } else {
+            const body = await response.json();
+        }
+    }
 
     return (
         <div>
